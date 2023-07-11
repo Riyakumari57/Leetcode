bae0573a13dd8893cd87c99647ecff58f15c253e
Riyakumari57 <65845230+Riyakumari57@users.noreply.github.com>
2023-07-11 20:04:16 +0530

    Time: 50 ms (23.53%), Space: 19.9 MB (26.86%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -11,38 +11,70 @@ public:
             return vv;
         }
          
-          sort(in.begin(),in.end());
-          int start = in[0][0];
-          int end = in[0][1];
-          cout<<start<<" "<<end<<endl;
-          for(int i= 1;i<in.size();i++)
-          {
-              vector<int>ans;
-                  if(in[i][0]<=end){
-                  end = max(in[i][1],end);
-                  }
-                  else{
-                      ans.push_back(start);
-                      ans.push_back(end);
-                      vv.push_back(ans);
-                      start = in[i][0];
-                      end = in[i][1];
-                      if(i==in.size()-1)
-                      {
-                          ans.clear();
-                           ans.push_back(start);
-                          ans.push_back(end);
-                          vv.push_back(ans);
-                          return vv;
-                      }
-                  }
+//           sort(in.begin(),in.end());
+//           int start = in[0][0];
+//           int end = in[0][1];
+//           cout<<start<<" "<<end<<endl;
+//           for(int i= 1;i<in.size();i++)
+//           {
+//               vector<int>ans;
+//                   if(in[i][0]<=end){
+//                   end = max(in[i][1],end);
+//                   }
+//                   else{
+//                       ans.push_back(start);
+//                       ans.push_back(end);
+//                       vv.push_back(ans);
+//                       start = in[i][0];
+//                       end = in[i][1];
+//                       if(i==in.size()-1)
+//                       {
+//                           ans.clear();
+//                            ans.push_back(start);
+//                           ans.push_back(end);
+//                           vv.push_back(ans);
+//                           return vv;
+//                       }
+//                   }
                  
                        
 
-          }
-                        v.push_back(start);
-                         v.push_back(end);
-                          vv.push_back(v);
-          return vv;
+//           }
+//                         v.push_back(start);
+//                          v.push_back(end);
+//                           vv.push_back(v);
+//           return vv;
+        
+         sort(in.begin(),in.end());
+        int start = in[0][0];
+        int end = in[0][1];
+        for(int i=1;i<in.size();i++)
+        {
+            vector<int>ans;
+             if(in[i][0]<=end)
+             {
+                 end = max(in[i][1] , end); 
+             }
+            else{
+                ans.push_back(start);
+                ans.push_back(end);
+                vv.push_back(ans);
+                start = in[i][0];
+                end = in[i][1];
+                
+                if(i==in.size()-1)
+                {
+                    ans.clear();
+                    ans.push_back(start);
+                    ans.push_back(end);
+                    vv.push_back(ans);
+                    return vv;
+                }
+            }
+        }
+        v.push_back(start);
+        v.push_back(end);
+        vv.push_back(v);
+        return vv;
     }
 };
