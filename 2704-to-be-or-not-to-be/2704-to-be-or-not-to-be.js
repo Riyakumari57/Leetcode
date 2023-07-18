4e5cf30d0e843879afd3518a2b66dc8ba9b49162
Riyakumari57 <65845230+Riyakumari57@users.noreply.github.com>
2023-07-18 18:42:23 +0530

    Time: 59 ms (54.63%), Space: 41.8 MB (63.64%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+   var obj = {
+        toBe : function(val1)
+        {
+            if(val===val1) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        
+        notToBe : function(val2)
+        {
+            if(val !==val2)
+                return true;
+            throw new Error("Equal");
+        }
+    }
+   
+   return obj;
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
