520085c0c132e6078373116670c69db0c7973c29
Riyakumari57 <65845230+Riyakumari57@users.noreply.github.com>
2023-07-19 09:42:40 +0530

    Time: 58 ms (75.50%), Space: 42.2 MB (76.69%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) 
+{
+   var val = init;
+    for(let i = 0 ; i<nums.length ; i++)
+        {
+            val = fn(val , nums[i]);
+        }
+    return val;
+};
