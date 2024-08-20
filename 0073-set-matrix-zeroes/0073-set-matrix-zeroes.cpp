e613fa5b49199fdb70d9c739dd5cc84d5b6e320a
Riya Kumari <riiyaasinghh@gmail.com>
2024-08-20 18:19:08 +0530

    Time: 24 ms (10.96%), Space: 16.9 MB (27.51%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix)
     {
+        
          vector< pair <int,int> > vect;
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j =0;j<matrix[i].size();j++)
-            {
-                  if(matrix[i][j]==0)
-                  {
-                      vect.push_back(make_pair(i,j));
-                  }
-            }
-        }
-         
-         for(int i=0;i<matrix.size();i++)
-        {
-            for(int j =0;j<matrix[i].size();j++)
-            {
-                for(int k = 0 ;k <vect.size();k++)
+         for(int i = 0 ; i < matrix.size() ; i ++){
+             for(int j = 0  ; j < matrix[0].size() ; j++){
+                   if(matrix[i][j] == 0){
+                 vect.push_back({i , j});
+               }
+             }
+            
+         }
+        
+         for(int i = 0 ; i < matrix.size() ; i ++){
+             for(int j = 0  ; j < matrix[0].size() ; j++){
+                   for(int k = 0 ;k <vect.size();k++)
                 {
                    
                      if(i== vect[k].first)
                      {
-                          //cout<< vect[k].first<<" "<<vect[k].second<<endl;
                          matrix[i][j]=0;
                      }
                      if(j==vect[k].second)
@@ -31,21 +27,7 @@ public:
                            matrix[i][j]=0;
                      }
                 }
-                 
-                
-            }
-
-        }
-
-
-
-         for(int i=0;i<matrix.size();i++)
-        {
-            for(int j =0;j<matrix[i].size();j++)
-            {
-                cout<<matrix[i][j];
-            }
-        }
-
+             }
+         }
     }
 };
